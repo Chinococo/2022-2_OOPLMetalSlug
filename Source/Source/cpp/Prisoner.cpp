8766dc7c705039feeec3e36f2e04bad21db92fbf
KenChen9 <59657465+KenChen9@users.noreply.github.com>
2023-05-22 20:02:23 +0800

    Prisoner rework

--- Source/Source/cpp/Prisoner.cpp
+++ Source/Source/cpp/Prisoner.cpp
@@ -2,6 +2,203 @@
 #include "../header/GameStorage.h"
 
 Prisoner::Prisoner(int x, int y) : Character(x, y, velocityX) {
+	anchoredX = x;
+}
+
+void Prisoner::init() {
+	std::vector<std::vector<std::string>> csv = readCSV("resources/csv/character.csv");
+	std::vector<std::string> paths;
+	std::pair<int, int> range;
+
+	for (size_t i = 1; i < csv.size(); i++) {
+		if (csv[i][0] != "prisoner")
+			continue;
+
+		int delay = std::stoi(csv[i][3]);
+		std::string prefix = csv[i][2];
+		range = { std::stoi(csv[i][1].substr(0, csv[i][1].find('~'))), std::stoi(csv[i][1].substr(csv[i][1].find('~') + 1)) };
+
+		for (int i = 0; i < range.second - range.first; i++) {
+			paths.push_back(prefix + std::to_string(i) + ".bmp");
+		}
+
+		animationRanges.push_back(range);
+		animationDelays.push_back(delay);
+	}
+
+	animationflipBias = range.second;
+
+	// filp
+	for (size_t i = 1; i < csv.size(); i++) {
+		if (csv[i][0] != "prisoner")
+			continue;
+
+		int delay = std::stoi(csv[i][3]);
+		std::string prefix = csv[i][2];
+		range = { std::stoi(csv[i][1].substr(0, csv[i][1].find('~'))), std::stoi(csv[i][1].substr(csv[i][1].find('~') + 1)) };
+
+		for (int i = 0; i < range.second - range.first; i++) {
+			paths.push_back(prefix + "flip_" + std::to_string(i) + ".bmp");
+		}
+
+		animationRanges.push_back(range);
+		animationDelays.push_back(delay);
+	}
+
+	LoadBitmapByString(paths, RGB(0, 0, 0));
+	animationRange = animationRanges[static_cast<int>(sprite)];
+	animationDelay = animationDelays[static_cast<int>(sprite)];
+}
+
+void Prisoner::update() {
+	if (!alive) {
+		return;
+	}
+
+	switch (action) {
+	case Action::TIED:
+		handleTied();
+		break;
+	case Action::RESCUED:
+		handleRescued();
+		break;
+	case Action::MOVE:
+		handleMove();
+		break;
+	case Action::REWARD:
+		handleReward();
+		break;
+	case Action::LEAVE:
+		handleLeave();
+		break;
+	}
+
+	// Handle animation
+	changeAnimation();
+	updateAnimation();
+}
+
+void Prisoner::draw() {
+	if (alive) {
+		ShowBitmap();
+	}
+	else {
+		UnshowBitmap();
+	}
+}
+
+void Prisoner::handleTied() {
+	// Set sprite and action for this state
+	sprite = Sprite::TIED;
+	action = Action::TIED;
+
+	// Switch to next action
+	bool rescuredByHeroKnife = IsOverlap(*this, marco) && marco.isAttacking();
+	bool rescuredByHeroBullet = false;
+
+	for (size_t i = 0; i < bullets.size(); i++) {
+		if (bullets[i].owner == "hero" && IsOverlap(*this, bullets[i])) {
+			rescuredByHeroBullet = true;
+			break;
+		}
+	}
+
+	if (rescuredByHeroKnife || rescuredByHeroBullet) {
+		sprite = Sprite::RESCUED;
+		action = Action::RESCUED;
+	}
+}
+
+void Prisoner::handleRescued() {
+
+}
+
+void Prisoner::handleMove() {
+
+}
+
+void Prisoner::handleReward() {
+
+}
+
+void Prisoner::handleLeave() {
+
+}
+
+void Prisoner::collideWithGround() {
+	// Check for falling
+	inAir = true;
+
+	for (size_t i = 0; i < grounds.size(); i++) {
+		if (Ground::isOnGround(*this, grounds[i]) == 1 && velocityY > 0) {
+			dy = Ground::GetX_Height(grounds[i], abs(ViewPointX) + x) - GetHeight() - y + ViewPointY - ViewPointYInit;
+
+			if (abs(dy) > 40) // Prevent lifting by steep wall
+				dy = 0;
+
+			// Stop falling
+			inAir = false;
+			velocityY = 0;
+
+			sprite = Sprite::FALL;
+			break;
+		}
+	}
+}
+
+void Prisoner::collideWithWall() {
+	for (size_t i = 0; i < grounds.size(); i++) {
+		if (dx > 0 && Ground::isOnGroundLeft(*this, grounds[i]) == 1) {
+			dx = 0;
+		}
+		else if (dx < 0 && Ground::isOnGroundRight(*this, grounds[i]) == 1) {
+			dx = 0;
+		}
+	}
+}
+
+void Prisoner::collideWithBorder() {
+	if (x + dx < 0) {
+		dx = -x;
+	}
+	else if (x + dx > 800) {
+		dx = 800 - x;
+	}
+
+	if ((action != Action::LEAVE) && (y + dy < 0)) {
+		dy = -y;
+	}
+	else if (y + dy > 600) {
+		dy = 600 - y;
+	}
+}
+
+void Prisoner::changeAnimation() {
+	animationRange = animationRanges[static_cast<int>(sprite)];
+	animationDelay = animationDelays[static_cast<int>(sprite)];
+	SetFrameIndexOfBitmap(animationRange.first + ((flip) ? animationflipBias : 0));
+}
+
+void Prisoner::updateAnimation() {
+	if (clock() - start > animationDelay) {
+		int frameIndex = GetFrameIndexOfBitmap();
+		int animationBias = (flip) ? animationflipBias : 0;
+		int totalFrames = animationRange.second - animationRange.first;
+		int newFrameIndex = (frameIndex - animationRange.first - animationBias + 1) % totalFrames;
+		int frameOffset = newFrameIndex + animationRange.first + animationBias;
+		SetFrameIndexOfBitmap(frameOffset);
+
+		bool reachedLastFrame = (frameOffset - animationRange.first) % totalFrames == 0;
+		once = reachedLastFrame;
+
+		start = clock();
+	}
+}
+
+
+
+/*
+Prisoner::Prisoner(int x, int y) : Character(x, y, velocityX) {
 	this->x = x;
 	this->y = y;
 	anchoredX = x;
@@ -106,7 +303,7 @@ void Prisoner::init() {
 	}
 	animationflipBias = range.second;
 
-	/* filp */
+	// filp
 	for (unsigned i = 1; i < csv.size(); i++) {
 		if (csv[i][0] != "prisoner")
 			continue;
@@ -209,14 +406,6 @@ void Prisoner::changeAnimation() {
 
 void Prisoner::updateAnimation() {
 	if (clock() - start > animationDelay) {
-		SetFrameIndexOfBitmap(((GetFrameIndexOfBitmap() - animationRange.first - +((flip) ? animationflipBias : 0) + 1) % (animationRange.second - animationRange.first)) + animationRange.first + ((flip) ? animationflipBias : 0));
-		if ((GetFrameIndexOfBitmap() - animationRange.first - +((flip) ? animationflipBias : 0)) % (animationRange.second - animationRange.first) == 0)
-			once = true;
-		start = clock();
-	}
-
-	/* Modified but untested code
-	if (clock() - start > animationDelay) {
 		int frameIndex = GetFrameIndexOfBitmap();
 		int animationBias = (flip) ? animationflipBias : 0;
 		int totalFrames = animationRange.second - animationRange.first;
@@ -229,7 +418,6 @@ void Prisoner::updateAnimation() {
 
 		start = clock();
 	}
-	*/
 }
 
 void Prisoner::collideWithGround() {
@@ -291,3 +479,4 @@ void Prisoner::draw() {
 void Prisoner::updateAction() {
 	// Pattern changed, no need for this method
 }
+*/
--- Source/Source/header/Prisoner.h
+++ Source/Source/header/Prisoner.h
@@ -1,43 +1,49 @@
 #pragma once
 #include "Character.h"
 
+enum class Direction {
+	LEFT, RIGHT, UP, DOWN
+};
+
 class Prisoner : public Character {
 private:
 	const int WANDER_DISTANCE = 200;
+	
 	int anchoredX = 0;
+	
 	int velocityX = 2;
 	int velocityY = 0;
+	
 	bool inAir = false;
 	bool once = true;
-	std::string direction = "left";
+	
 	enum class Sprite {
 		TIED, RESCUED, MOVE, FALL, REWARD
 	} sprite = Sprite::TIED;
-	
-public:
+
 	enum class Action {
 		TIED, RESCUED, MOVE, REWARD, LEAVE
 	} action = Action::TIED;
-	// Should've split movement logic into methods
-	// This will be refactored in the future update
-	Prisoner(int x, int y);
-	virtual void init() override;
-	virtual void update() override;
-	virtual void control() override;
-	virtual void move() override;
-	virtual void moveLeftRight() override;
-	virtual void jumpAndFall() override;
-	virtual void updateAction() override;
-	virtual void changeAnimation() override;
-	virtual void updateAnimation() override;
-	virtual void collideWithGround() override;
-	virtual void collideWithWall() override;
-	void collideWithBorder();
-	virtual void draw() override;
-	// Main action logic
+
+	Direction direction = Direction::RIGHT;
+
 	void handleTied();
 	void handleRescued();
 	void handleMove();
 	void handleReward();
 	void handleLeave();
+
+	virtual void collideWithGround() override;
+	virtual void collideWithWall() override;
+	void collideWithBorder();
+
+	virtual void changeAnimation() override;
+	virtual void updateAnimation() override;
+	
+public:
+	Prisoner(int x, int y);
+	
+	virtual void init() override;
+	virtual void update() override;
+	virtual void draw() override;
 };
